3cf2b0f1805ea3624b529c022a71f27fef2c5533
YPZ <YPZ@mail.com>
2023-02-01 17:08:23 +0800

    查看商品详情

---
+++ pages/product_detail/product_detail.js
@@ -0,0 +1,137 @@
+// pages/product_detail/product_detail.js
+// 导入后端请求公共类
+import {
+  requestPubUtil,
+  getBaseUrl
+} from "../../utils/requestPubUtil.js"
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // 请求后端根路径
+    baseUrl: '',
+    // 商品对象
+    product: {},
+    // 点击事件切换索引
+    activeIndex: 0,
+  },
+  productInfo: {
+
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    /**
+     * 获取后端请求根路径
+     */
+    const baseUrl = getBaseUrl();
+    this.setData({
+      baseUrl: baseUrl
+    })
+    // console.log("id=" + options.id); 
+    this.getProductDetail(options.id);
+  },
+
+  // 获取商品详情
+  async getProductDetail(id) {
+    const result = await requestPubUtil({
+      url: '/product/queryProductDetail?id=' + id,
+      // data: {
+      //   id
+      // },
+      method: "GET"
+    });
+    this.productInfo = result.data.datas;
+    this.setData({
+      product: result.data.datas
+    })
+  },
+
+  // tab点击事件
+  handleItemTap(e) {
+    const index = e.currentTarget.dataset.index;
+    console.log(index)
+    this.setData({
+      activeIndex: index
+    })
+  },
+
+
+  // 点击事件 商品加入购物车
+  handleCartAdd() {
+
+    this.setCartadd();
+
+    wx.showToast({
+      title: '加入成功',
+      icon: 'success',
+      mask: true
+    })
+  },
+
+  // 加入购物车
+  setCartadd() {
+    let cart = wx.getStorageSync('cart') || [];
+    console.log("cart=" + cart);
+    let index = cart.findIndex(v => v.id === this.productInfo.id);
+    if (index === -1) { // 购物车里面不存在当前商品 
+      this.productInfo.num = 1;
+      cart.push(this.productInfo);
+    } else { // 已经存在
+      cart[index].num++;
+    }
+    wx.setStorageSync('cart', cart); // 把购物车添加到缓存中
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
--- utils/requestPubUtil.js
+++ utils/requestPubUtil.js
@@ -4,7 +4,9 @@
  */
 
 // 定义请求根路径baseUrl
-const baseUrl = "http://localhost:8088"
+const baseUrl = "http://localhost:8088";
+// 同时并发请求的次数
+let ajaxTimes = 0;
 /**
  * 
  * @param {返回请求根路径} params 
@@ -13,12 +15,26 @@ export const getBaseUrl = () => {
   return baseUrl;
 }
 export const requestPubUtil = (params) => {
+  var start = new Date().getTime();
+  console.log(start)
+
+  ajaxTimes++;
+
+  wx.showLoading({
+    title: '加载中...',
+    mask: true
+  })
+
+  // 模拟网络延迟加载...
+  while (true) {
+    if (new Date().getTime() - start > 1 * 100) break;
+  }
   return new Promise((resolve, reject) => {
     // 发送异步请求
     wx.request({
       // 解构请求的所有参数（请求路径及请求方式）
-      url:baseUrl+params.url,
-      method:params.method,
+      url: baseUrl + params.url,
+      method: params.method,
       // 请求成功调用resolve方法（fullfilled状态）
       success: (result) => {
         resolve(result)
@@ -26,6 +42,13 @@ export const requestPubUtil = (params) => {
       // 请求失败调用reject方法（rejected状态）
       fail: (err) => {
         reject(err)
+      },
+      complete: () => {
+        ajaxTimes--;
+        if (ajaxTimes == 0) {
+          wx.hideLoading(); // 关闭加载图标
+        }
+
       }
     })
   });
