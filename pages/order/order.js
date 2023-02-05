5a99a7c912ee510a7e7b1cfe8b21a368dc2fcf56
YPZ <YPZ@mail.com>
2023-02-05 16:19:35 +0800

    小程序端完成

--- pages/order/order.js
+++ pages/order/order.js
@@ -1,66 +1,135 @@
-// pages/order/order.js
+// 导入request请求工具类
+import {
+  getBaseUrl,
+  requestPubUtil
+} from '../../utils/requestPubUtil.js';
+import regeneratorRuntime from '../../lib/runtime/runtime';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    orders: [],
+    tabs: [{
+        id: 0,
+        value: "全部订单",
+        isActive: true
+      },
+      {
+        id: 1,
+        value: "待付款",
+        isActive: false
+      },
+      {
+        id: 2,
+        value: "待收货",
+        isActive: false
+      },
+      {
+        id: 3,
+        value: "退款/退货",
+        isActive: false
+      },
+    ]
+  },
 
+  // 接口参数
+  QueryParams: {
+    type: 0
+  },
+
+  /**
+   * tab点击事件处理
+   * @param {} e 
+   */
+  handleItemTap(e) {
+    const {
+      index
+    } = e.currentTarget.dataset;
+    console.log(index)
+    // 切换标题
+    let {
+      tabs
+    } = this.data;
+    tabs.forEach((v, i) => i == index ? v.isActive = true : v.isActive = false);
+
+    // 获取订单列表
+    this.QueryParams.type = index;
+    this.getOrders();
+
+    this.setData({
+      tabs
+    })
+  },
+
+  /**
+   * 获取订单
+   */
+  async getOrders() {
+    const res = await requestPubUtil({
+      url: '/my/order/queryAllOrder',
+      data: this.QueryParams
+    });
+    console.log(res)
+    this.setData({
+      orders: res.data.datas
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {
+  onLoad: function (options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() {
+  onReady: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow() {
-
+  onShow: function () {
+    this.getOrders();
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload() {
+  onUnload: function () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage() {
+  onShareAppMessage: function () {
 
   }
 })
--- pages/search/search.js
+++ pages/search/search.js
@@ -1,66 +1,122 @@
-// pages/search/search.js
+// 导入request请求工具类
+import {
+  getBaseUrl,
+  requestPubUtil
+} from '../../utils/requestPubUtil.js';
+import regeneratorRuntime from '../../lib/runtime/runtime';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    productList: [], // 商品数组
+    inputValue: "", // 输入框的值
+    isFocus: false // 取消 按钮 是否显示
+  },
 
+  TimeoutId: -1,
+
+  // 处理input事件
+  handleInput(e) {
+    const {
+      value
+    } = e.detail;
+    if (!value.trim()) {
+      this.setData({
+        productList: [],
+        isFocus: false
+      })
+      return;
+    }
+    this.setData({
+      isFocus: true
+    })
+    clearTimeout(this.TimeoutId);
+    this.TimeoutId = setTimeout(() => {
+      this.search(value)
+    }, 1000)
+
+  },
+
+  /**
+   * 获取商品详情
+   */
+  async search(queryParam) {
+    const result = await requestPubUtil({
+      url: '/product/search',
+      data: {
+        queryParam
+      }
+    });
+    console.log(result)
+    this.setData({
+      productList: result.data.datas
+    })
+  },
+
+  // 点击 取消按钮
+  handleCancel() {
+    this.setData({
+      productList: [], // 商品数组
+      inputValue: "", // 输入框的值
+      isFocus: false // 取消 按钮 是否显示
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {
+  onLoad: function (options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() {
+  onReady: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow() {
+  onShow: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload() {
+  onUnload: function () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage() {
+  onShareAppMessage: function () {
 
   }
 })
