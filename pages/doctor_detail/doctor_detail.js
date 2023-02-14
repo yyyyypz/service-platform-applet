47a09b5bda359df9829234dab6308cd8af90762c
YPZ <YPZ@mail.com>
2023-02-14 22:07:54 +0800

    完成挂号预约功能

--- pages/doctor_detail/doctor_detail.js
+++ pages/doctor_detail/doctor_detail.js
@@ -1,4 +1,4 @@
-// pages/catagory/catagory.js
+// pages/product_detail/product_detail.js
 // 导入后端请求公共类
 import {
   requestPubUtil,
@@ -10,26 +10,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-    // 左侧商品大类
-    leftBigType: [],
-    // 右侧商品小类
-    rightSmallType: [],
+    // 请求后端根路径
     baseUrl: '',
-    // 当前被选中索引
-    currentIndex: 0,
-    // 右侧滚动置顶
-    scrollTop: 0,
+    // 商品对象
     product: {},
     // 点击事件切换索引
     activeIndex: 0,
   },
-
   productInfo: {
 
   },
-
-  allData: [],
-
   /**
    * 生命周期函数--监听页面加载
    */
@@ -41,45 +31,14 @@ Page({
     this.setData({
       baseUrl: baseUrl
     })
-    this.getAllData();
-    // this.getProductDetail(options.id);
-  },
-
-  // 获取所有商品分类
-  async getAllData() {
-    const result = await requestPubUtil({
-      url: '/hospital/queryBook',
-      method: "GET"
-    });
-    // 用allData接收所有数据
-    this.allData = result.data.datas;
-    // 用map遍历allData并且只获取大类中的name
-    let leftBigType = this.allData.map((value) => {
-      return value.name;
-    })
-    // 获取小类当中的商品数据
-    let rightSmallType = this.allData[0].medicalTypeList;
-    this.setData({
-      leftBigType: leftBigType,
-      rightSmallType: rightSmallType
-    })
-  },
-
-  leftMenuChange(e) {
-    // 左侧商品大类索引点击后切换
-    const index = e.target.dataset.index;
-    let rightSmallType = this.allData[index].medicalTypeList;
-    this.setData({
-      currentIndex: index,
-      rightSmallType: rightSmallType,
-      scrollTop: 0
-    })
+    // console.log("id=" + options.id); 
+    this.getProductDetail(options.id);
   },
 
   // 获取商品详情
   async getProductDetail(id) {
     const result = await requestPubUtil({
-      url: '/house/queryHouseDetail?id=' + id,
+      url: '/doctor/queryDoctorDetail?id=' + id,
       method: "GET"
     });
     this.productInfo = result.data.datas;
@@ -112,18 +71,17 @@ Page({
 
   // 加入购物车
   setCartadd() {
-    let my_book = wx.getStorageSync('my_book') || [];
-    console.log("my_book=" + my_book);
-    let index = my_book.findIndex(v => v.id === this.productInfo.id);
-    this.getProductDetail(index);
+    let booking = wx.getStorageSync('booking') || [];
+    console.log("booking=" + booking);
+    let index = booking.findIndex(v => v.id === this.productInfo.id);
     if (index === -1) { // 购物车里面不存在当前商品 
       this.productInfo.num = 1;
       this.productInfo.checked = true;
-      my_book.push(this.productInfo);
+      booking.push(this.productInfo);
     } else { // 已经存在
-      my_book[index].num++;
+      booking[index].num++;
     }
-    wx.setStorageSync('my_book', my_book); // 把购物车添加到缓存中
+    wx.setStorageSync('booking', booking); // 把购物车添加到缓存中
   },
 
   /**
